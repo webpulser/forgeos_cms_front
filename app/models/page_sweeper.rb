d5bebc2718af7270620683f9be89015af7b33cbd
Sebastien Deloor <seb@SebMac-2.local>
2011-08-30 12:27:10 +0200

    fix page_sweeper page path

--- app/models/page_sweeper.rb
+++ app/models/page_sweeper.rb
@@ -2,7 +2,7 @@ class PageSweeper < ActionController::Caching::Sweeper
 
   def expire_cache_for_page(page)
     page.page_urls.each do |url|
-      expire_page forgeos_cms.page_path(url)
+      expire_page forgeos_cms_front.page_path(url)
     end
 
     page.menu_links.each do |link|
